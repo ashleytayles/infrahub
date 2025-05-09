c91a570d38071ae063375cc1757b7b3ca6323251
Fatih Acar <fatih@opsmill.com>
2025-05-09 09:18:06 +0200

    fix: small test
    Signed-off-by: Fatih Acar <fatih@opsmill.com>

--- backend/infrahub/core/constraint/node/runner.py
+++ backend/infrahub/core/constraint/node/runner.py
@@ -26,16 +26,19 @@ class NodeConstraintRunner:
     async def check(
         self, node: Node, field_filters: list[str] | None = None, skip_uniqueness_check: bool = False
     ) -> None:
-        async with self.db.start_session(read_only=False) as db:
-            await node.resolve_relationships(db=db)
+        async with self.db.start_session() as db:
+            async with self.db.start_session(read_only=True) as dbro:
+                await node.resolve_relationships(db=db)
 
-            if not skip_uniqueness_check:
-                await self.uniqueness_constraint.check(node, filters=field_filters)
+                if not skip_uniqueness_check:
+                    self.uniqueness_constraint.db = dbro
+                    await self.uniqueness_constraint.check(node, filters=field_filters)
 
-            for relationship_name in node.get_schema().relationship_names:
-                if field_filters and relationship_name not in field_filters:
-                    continue
-                relationship_manager: RelationshipManager = getattr(node, relationship_name)
-                await relationship_manager.fetch_relationship_ids(db=db, force_refresh=True)
-                for relationship_constraint in self.relationship_manager_constraints:
-                    await relationship_constraint.check(relm=relationship_manager, node_schema=node.get_schema())
+                for relationship_name in node.get_schema().relationship_names:
+                    if field_filters and relationship_name not in field_filters:
+                        continue
+                    relationship_manager: RelationshipManager = getattr(node, relationship_name)
+                    await relationship_manager.fetch_relationship_ids(db=dbro, force_refresh=True)
+                    for relationship_constraint in self.relationship_manager_constraints:
+                        relationship_constraint.db = dbro
+                        await relationship_constraint.check(relm=relationship_manager, node_schema=node.get_schema())
